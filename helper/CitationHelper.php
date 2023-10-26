ae3e38a161aa33787245e67900ddcc0b18fc881b
Joseph Anderson <joseph_anderson@fitnyc.edu>
2023-10-26 10:42:29 -0400

    more pink

--- helper/CitationHelper.php
+++ helper/CitationHelper.php
@@ -6,78 +6,78 @@ use Omeka\Api\Representation\ItemRepresentation;
 
 class CitationHelper extends AbstractHelper
 {
-    public function __invoke(ItemRepresentation $item)
-    {
-        $escape = $this->getView()->plugin('escapeHtml');
-        $apaAuthors = '';
-        $chicagoAuthors = '';
-        $mlaAuthors = '';
-        $title = $item->displayTitle();
-        $titleCased = $this->titleCase($title);
-        $date = $item->value('dcterms:date');
-        $thesis = '';
-        $chicagoThesis = 'FIT Digital Repository';
-        $mlaThesis = '<em>FIT Digital Repository</em>';
-        if ($types = $item->value('dcterms:type', ['all' => true])) {
-            foreach ($types as $type) {
-                if (ucfirst($escape($type)) == "Thesis") {
-                    $thesis = ' [Master\'s thesis, Fashion Institute of Technology, State University of New York]';
-                    $chicagoThesis = 'Master\'s thesis, Fashion Institute of Technology, State University of New York';
-                    $mlaThesis = 'Fashion Institute of Technology, State University of New York, Master\'s thesis. <em>FIT Digital Repository</em>';
-                }
-            }
+  public function __invoke(ItemRepresentation $item)
+  {
+    $escape = $this->getView()->plugin('escapeHtml');
+    $apaAuthors = '';
+    $chicagoAuthors = '';
+    $mlaAuthors = '';
+    $title = $item->displayTitle();
+    $titleCased = $this->titleCase($title);
+    $date = $item->value('dcterms:date');
+    $thesis = '';
+    $chicagoThesis = 'FIT Digital Repository';
+    $mlaThesis = '<em>FIT Digital Repository</em>';
+    if ($types = $item->value('dcterms:type', ['all' => true])) {
+      foreach ($types as $type) {
+        if (ucfirst($escape($type)) == "Thesis") {
+          $thesis = ' [Master\'s thesis, Fashion Institute of Technology, State University of New York]';
+          $chicagoThesis = 'Master\'s thesis, Fashion Institute of Technology, State University of New York';
+          $mlaThesis = 'Fashion Institute of Technology, State University of New York, Master\'s thesis. <em>FIT Digital Repository</em>';
         }
-        $url = $escape($item->url());
-        if ($contributors = $item->value('dcterms:contributor', ['all' => true])) {
-            $authorList = [];
-            foreach ($contributors as $key => $contributor) {
-                if (str_contains($contributor->asHtml(), 'Author') || str_contains($contributor->asHtml(), 'Creator')) {
-                    if (!in_array($contributor, $authorList)) {
-                        $authorList[] = $contributor;
-                    }
-                }
-            }
-            if (empty($authorList)) {
-                $authorList[] = $contributors[0];
-            }
-            $len = count($authorList);
-            foreach ($authorList as $authorKey => $author) {
-                $lastName = trim(explode(",", $author)[0]);
-                $firstAndMiddle = trim(explode(",", $author)[1]);
-                $firstAndMiddleArray = explode(' ', $firstAndMiddle);
-                $initials = '';
-                foreach ($firstAndMiddleArray as $initialKey => $part) {
-                    if ($initialKey == 0) {
-                        $initials .= strtoupper($part[0]) . '.';
-                    } else {
-                        $initials .= ' ' . strtoupper($part[0]) . '.';
-                    }
-                }
-                if ($authorKey == 0) {
-                    $apaAuthors .= $lastName . ', ' . $initials;
-                    $chicagoAuthors .= $author;
-                    $mlaAuthors .= $author;
-                } elseif (($len - $authorKey) == 1) {
-                    $apaAuthors .= ', & ' . $lastName . ', ' . $initials;
-                    $chicagoAuthors .= ', and ' . $firstAndMiddle . ' ' . $lastName;
-                    $mlaAuthors .= ', and ' . $firstAndMiddle . ' ' . $lastName;
-                } else {
-                    $apaAuthors .= ', ' . $lastName . ', ' . $initials;
-                    $chicagoAuthors .= ', ' . $firstAndMiddle . ' ' . $lastName;
-                }
-                if ($len > 2) {
-                    $mlaAuthors = $authorList[0] . ', et al';
-                }
-            }
-            $apa = $apaAuthors . ' (' . $date . '). <em>' . $title . '</em>' . $thesis . '. FIT Institutional Repository. ' . $url;
-            $chicago = $chicagoAuthors . '. "' . $titleCased . '." ' . $chicagoThesis . ', ' . $date . '. ' . $url;
-            $mla = $mlaAuthors . '. <em>' . $titleCased . '</em>. ' . $date . '. ' . $mlaThesis . ', ' . $url;
+      }
+    }
+    $url = $escape($item->url());
+    if ($contributors = $item->value('dcterms:contributor', ['all' => true])) {
+      $authorList = [];
+      foreach ($contributors as $key => $contributor) {
+        if (str_contains($contributor->asHtml(), 'Author') || str_contains($contributor->asHtml(), 'Creator')) {
+          if (!in_array($contributor, $authorList)) {
+            $authorList[] = $contributor;
+          }
+        }
+      }
+      if (empty($authorList)) {
+        $authorList[] = $contributors[0];
+      }
+      $len = count($authorList);
+      foreach ($authorList as $authorKey => $author) {
+        $lastName = trim(explode(",", $author)[0]);
+        $firstAndMiddle = trim(explode(",", $author)[1]);
+        $firstAndMiddleArray = explode(' ', $firstAndMiddle);
+        $initials = '';
+        foreach ($firstAndMiddleArray as $initialKey => $part) {
+          if ($initialKey == 0) {
+            $initials .= strtoupper($part[0]) . '.';
+          } else {
+            $initials .= ' ' . strtoupper($part[0]) . '.';
+          }
+        }
+        if ($authorKey == 0) {
+          $apaAuthors .= $lastName . ', ' . $initials;
+          $chicagoAuthors .= $author;
+          $mlaAuthors .= $author;
+        } elseif (($len - $authorKey) == 1) {
+          $apaAuthors .= ', & ' . $lastName . ', ' . $initials;
+          $chicagoAuthors .= ', and ' . $firstAndMiddle . ' ' . $lastName;
+          $mlaAuthors .= ', and ' . $firstAndMiddle . ' ' . $lastName;
         } else {
-            $apa = '<em>' . $title . '</em>' . $thesis . '. (' . $date . ')' . '. FIT Institutional Repository. ' . $url;
-            $chicago = '"' . $titleCased . '." ' . $chicagoThesis . ', ' . $date . '. ' . $url;
-            $mla = '<em>' . $titleCased . '</em>. ' . $date . '. ' . $mlaThesis . ', ' . $url;
+          $apaAuthors .= ', ' . $lastName . ', ' . $initials;
+          $chicagoAuthors .= ', ' . $firstAndMiddle . ' ' . $lastName;
         }
-        return '
+        if ($len > 2) {
+          $mlaAuthors = $authorList[0] . ', et al';
+        }
+      }
+      $apa = $apaAuthors . ' (' . $date . '). <em>' . $title . '</em>' . $thesis . '. FIT Institutional Repository. ' . $url;
+      $chicago = $chicagoAuthors . '. "' . $titleCased . '." ' . $chicagoThesis . ', ' . $date . '. ' . $url;
+      $mla = $mlaAuthors . '. <em>' . $titleCased . '</em>. ' . $date . '. ' . $mlaThesis . ', ' . $url;
+    } else {
+      $apa = '<em>' . $title . '</em>' . $thesis . '. (' . $date . ')' . '. FIT Institutional Repository. ' . $url;
+      $chicago = '"' . $titleCased . '." ' . $chicagoThesis . ', ' . $date . '. ' . $url;
+      $mla = '<em>' . $titleCased . '</em>. ' . $date . '. ' . $mlaThesis . ', ' . $url;
+    }
+    return '
         <ul class="nav nav-tabs mb-3" id="citationTab" role="tablist">
           <li class="nav-item" role="presentation">
             <button class="nav-link active text-dark" id="apa-tab" data-bs-toggle="tab" data-bs-target="#apa" type="button" role="tab" aria-controls="apa" aria-selected="true">APA</button>
@@ -95,7 +95,7 @@ class CitationHelper extends AbstractHelper
               <div class="form-control font-monospace text-break" id="apaCitation">
               ' . str_replace('..', '.', $apa) . '
               </div>
-              <button class="btn btn-secondary clip-button" type="button" id="apa-button" data-clipboard-target="#apaCitation" aria-label="Copy citation to clipboard">
+              <button class="btn btn-dark clip-button" type="button" id="apa-button" data-clipboard-target="#apaCitation" aria-label="Copy citation to clipboard">
                 <i class="fas fa-copy" title="Copy citation to clipboard" aria-hidden="true"></i>
               </button>
             </div>
@@ -105,7 +105,7 @@ class CitationHelper extends AbstractHelper
             <div class="form-control font-monospace text-break" id="mlaCitation">
             ' . str_replace('..', '.', $mla) . '
             </div>
-            <button class="btn btn-secondary clip-button" type="button" id="mla-button" data-clipboard-target="#mlaCitation" aria-label="Copy citation to clipboard">
+            <button class="btn btn-dark clip-button" type="button" id="mla-button" data-clipboard-target="#mlaCitation" aria-label="Copy citation to clipboard">
               <i class="fas fa-copy" title="Copy citation to clipboard" aria-hidden="true"></i>
             </button>
           </div>
@@ -115,7 +115,7 @@ class CitationHelper extends AbstractHelper
             <div class="form-control font-monospace text-break" id="chicagoCitation">
             ' . str_replace('..', '.', $chicago) . '
             </div>
-            <button class="btn btn-secondary clip-button" type="button" id="chicago-button" data-clipboard-target="#chicagoCitation" aria-label="Copy citation to clipboard">
+            <button class="btn btn-dark clip-button" type="button" id="chicago-button" data-clipboard-target="#chicagoCitation" aria-label="Copy citation to clipboard">
               <i class="fas fa-copy" title="Copy citation to clipboard" aria-hidden="true"></i>
             </button>
           </div>
@@ -123,29 +123,52 @@ class CitationHelper extends AbstractHelper
         </div>
 
         ';
+  }
+  // Converts $title to Title Case, and returns the result.
+  protected function titleCase($title)
+  {
+    $smallwordsarray = array(
+      'of',
+      'a',
+      'the',
+      'and',
+      'an',
+      'or',
+      'nor',
+      'but',
+      'is',
+      'if',
+      'then',
+      'else',
+      'when',
+      'at',
+      'from',
+      'by',
+      'on',
+      'off',
+      'for',
+      'in',
+      'out',
+      'over',
+      'to',
+      'into',
+      'with'
+    );
+
+    // Split the string into separate words
+    $words = explode(' ', $title);
+
+    foreach ($words as $key => $word) {
+      // If this word is the first, or it's not one of our small words, capitalise it
+      // with ucwords().
+      if ($key == 0 or !in_array($word, $smallwordsarray)) {
+        $words[$key] = ucwords($word);
+      }
     }
-    // Converts $title to Title Case, and returns the result.
-    protected function titleCase($title)
-    {
-        $smallwordsarray = array(
-      'of','a','the','and','an','or','nor','but','is','if','then','else','when',
-      'at','from','by','on','off','for','in','out','over','to','into','with'
-      );
 
-        // Split the string into separate words
-        $words = explode(' ', $title);
+    // Join the words back into a string
+    $newtitle = implode(' ', $words);
 
-        foreach ($words as $key => $word) {
-            // If this word is the first, or it's not one of our small words, capitalise it
-            // with ucwords().
-            if ($key == 0 or !in_array($word, $smallwordsarray)) {
-                $words[$key] = ucwords($word);
-            }
-        }
-
-        // Join the words back into a string
-        $newtitle = implode(' ', $words);
-
-        return $newtitle;
-    }
+    return $newtitle;
+  }
 }
